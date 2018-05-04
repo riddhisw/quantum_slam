0f1a38ef522b66b58fcff610a49be79011167e37
riddhisw <riddhi.sw@gmail.com>
2018-05-04 14:39:11 +1000

    Added a simple demonstration

---
+++ preamble.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+import matplotlib.pyplot as plt 
+
+from qslam.slampf import ParticleFilter
+from qslam.mapping import TrueMap
+
+from contextlib import contextmanager
+
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:  
+            yield
+        finally:
+            sys.stdout = old_stdout
+
