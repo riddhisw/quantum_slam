Bug testing lengthscale posterior calculation in collapse of beta layer.This commit retains Data association functions under qslamrand has not moved them to the definition of a node

model_design.py
@@ -27,11 +27,11 @@ DICTIONARIES
 
 import numpy as np
 
-GRIDDICT = {"QUBIT_1" : (4., 3.5),
-            "QUBIT_2" : (1., 2.5),
-            "QUBIT_3" : (0., 3.5),
-            # "QUBIT_4" : (4.1, 0),
-            # "QUBIT_5" : (2.0, 2.5),
+GRIDDICT = { "QUBIT_1" : (1., 3.5),
+             "QUBIT_2" : (2., 3.5),
+             "QUBIT_3" : (3., 3.5),
+             "QUBIT_4" : (4., 3.5),
+             "QUBIT_5" : (5., 3.5),
             # "QUBIT_6" : (4., 1.5),
             # "QUBIT_7" : (2., 3.5),
             # "QUBIT_8" : (4., 2.3),
@@ -50,15 +50,15 @@ def gaussian_kernel(dist_jq, f_est_j, r_est_j):
 INITIALDICT = {"MU_W" : 0.0, # Qubit position noise mean (dynamics)
                "SIG2_W" : 1.0, # Qubit position noise variance (dynamics)
                "MU_R" : 0.0, # Length scale noise mean (dynamics)
-               "SIG2_R" : 1.0, # Length scale noise variance (dynamics)
+               "SIG2_R" : 0.1**2, # Length scale noise variance (dynamics)
                "MU_MEASR" : 0.0, # Map noise mean (measurement)
-               "SIG2_MEASR" : 1.0, # Map noise variance (measurement)
+               "SIG2_MEASR" : 0.000000000001, # Map noise variance (measurement)
                "MU_F" : 0.0, # True sigmoid approximation error mean
-               "SIG2_F" : 1.0, # True sigmoid approximation error variance
+               "SIG2_F" : 0.1*np.pi**2, # True sigmoid approximation error variance
                "LAMBDA" : 0.99, # Forgetting factor for quasi-msmt information
                "GAMMA_T" : 10**8, # Re-sampling threshold
                "P_ALPHA" : 3, # Number of alpha particles
-               "P_BETA" : 3, # Numer of beta particles for each alpha
+               "P_BETA" : 10, # Numer of beta particles for each alpha
                "kernel_function" : gaussian_kernel
               }
               

particledict.py
@@ -17,7 +17,7 @@ def likelihood_func_alpha(**args):
 
     msmt_dj = args["msmt_dj"]
     prob_j = args["prob_j"]
-    var_r = args["var_r"]
+    var_r = args["SIG2_MEASR"]
     rho_0 = rho(0.5, var_r)
 
     alpha_weight = rho_0 / 2.0
@@ -26,38 +26,39 @@ def likelihood_func_alpha(**args):
     elif msmt_dj == 1:
         alpha_weight += 1.0*rho_0*(2.0*prob_j - 1)
 
-    print
-    print "... ... ... ... In likelihood_func_alpha..."
-    print "... ... ... ... ... From >>>, physical msmt, at j,  msmt_dj = ", msmt_dj
-    print "... ... ... ... ... From >>>, sample probability , prob_j= ", prob_j
-    print "... ... ... ... ... From INITDICT, variance , var_r = ", var_r
-    print "... ... ... ... ... With analytic rho_0 for b=1/2 is rho_0= ", rho_0
-    print "... ... ... ... ... This gives a likelihood for alpha particle =", alpha_weight
-    print "... ... ... ... EXIT likelihood_func_alpha"
+    # print  
+    # print  "... ... ... ... In likelihood_func_alpha..."
+    # print  "... ... ... ... ... From >>>, physical msmt, at j,  msmt_dj = ", msmt_dj
+    # print  "... ... ... ... ... From >>>, sample probability , prob_j= ", prob_j
+    # print  "... ... ... ... ... From INITDICT, variance , var_r = ", var_r
+    # print  "... ... ... ... ... With analytic rho_0 for b=1/2 is rho_0= ", rho_0
+    # print  "... ... ... ... ... This gives a likelihood for alpha particle =", alpha_weight
+    # print  "... ... ... ... EXIT likelihood_func_alpha"
 
     return alpha_weight
 
 LIKELIHOOD_ALPHA = {"l_func" : likelihood_func_alpha,
-                    "l_args" : {"mu_R" : INITIALDICT["MU_R"], # TODO:
-                                "var_r" : INITIALDICT["SIG2_R"], # TODO:
+                    "l_args" : {"MU_MEASR" : INITIALDICT["MU_MEASR"], # TODO:
+                                "SIG2_MEASR" : INITIALDICT["SIG2_MEASR"], # TODO:
                                 "msmt_dj" : -10.0, # TODO: update via PF
                                 "prob_j" : -10.0 # TODO: update via PF
                                }}
 
 def alpha_weight_calc(alpha_particle_object, **args):
     '''docstring'''
-    print
-    print "... ... In alpha_weight_calc..."
+    # print 
+    # print  "... ... In alpha_weight_calc..."
 
     old_weight = alpha_particle_object.weight
+    # args["l_args"]["SIG2_MEASR"] = alpha_particle_object.SIG2_MEASR 
     likelihood = args["l_func"](**args["l_args"]) # TODO: there needs to be a better way of taking in msmts.
     new_raw_weight = old_weight*likelihood
 
-    print "... ... ..., old_weight", old_weight
-    print "... ... ..., likelihood", likelihood
-    print "... ... ..., new_raw_weight", new_raw_weight
-    print "... ... EXIT alpha_weight_calc"
-    print
+    # print  "... ... ..., old_weight", old_weight
+    # print  "... ... ..., likelihood", likelihood
+    # print  "... ... ..., new_raw_weight", new_raw_weight
+    # print  "... ... EXIT alpha_weight_calc"
+    # print 
     return new_raw_weight
 
 
@@ -65,15 +66,15 @@ WEIGHTFUNCDICT_ALPHA = {"function": alpha_weight_calc, "args": LIKELIHOOD_ALPHA}
 
 def update_alpha_dictionary(next_phys_msmt_j, prob_j):
     '''docstring'''
-    print
-    print " ... In update_alpha_dictionary ..."
-    print " ... ...  In LIKELIHOOD_ALPHA[msmt_dj] - old value - ", LIKELIHOOD_ALPHA["l_args" ]["msmt_dj"] 
-    print " ... ...  In LIKELIHOOD_ALPHA[prob_j] - old value - " , LIKELIHOOD_ALPHA["l_args" ]["prob_j"]
+    # print 
+    # print  " ... In update_alpha_dictionary ..."
+    # print  " ... ...  In LIKELIHOOD_ALPHA[msmt_dj] - old value - ", LIKELIHOOD_ALPHA["l_args" ]["msmt_dj"] 
+    # print  " ... ...  In LIKELIHOOD_ALPHA[prob_j] - old value - " , LIKELIHOOD_ALPHA["l_args" ]["prob_j"]
     LIKELIHOOD_ALPHA["l_args" ]["msmt_dj"] = next_phys_msmt_j
     LIKELIHOOD_ALPHA["l_args" ]["prob_j"] = prob_j
-    print " ... ...  In LIKELIHOOD_ALPHA[msmt_dj] - new value - ", LIKELIHOOD_ALPHA["l_args" ]["msmt_dj"] 
-    print " ... ...  In LIKELIHOOD_ALPHA[prob_j] - new value - ", LIKELIHOOD_ALPHA["l_args" ]["prob_j"]
-    print " ... EXIT update_alpha_dictionary ..."
+    # print  " ... ...  In LIKELIHOOD_ALPHA[msmt_dj] - new value - ", LIKELIHOOD_ALPHA["l_args" ]["msmt_dj"] 
+    # print  " ... ...  In LIKELIHOOD_ALPHA[prob_j] - new value - ", LIKELIHOOD_ALPHA["l_args" ]["prob_j"]
+    # print  " ... EXIT update_alpha_dictionary ..."
 
 ###############################################################################
 # BETA PARTICLES
@@ -88,13 +89,13 @@ def likelihood_func_beta(**args):
     prefactor = 1.0 / np.sqrt(2.0 * np.pi * variance)
     argument = -1.0 * ((new_phase - old_phase)- mean)**2 / (2.0 * variance)
     result = prefactor * np.exp(argument)
-    print
-    print "... ... ... ... In likelihood_func_beta ... with mean, variance = ", mean, variance
-    print "... ... ... ... ... The new_phase at q based on smearing from physical msmt info at j:", new_phase
-    print "... ... ... ... ... The old_phase at q based on posterior QubitGrid at q, at t-1:", old_phase
-    print "... ... ... ... ... Likelihood contribution at q: result = ", result
-    print "... ... ... ... EXIT likelihood_func_beta ..."
-    print
+    # print 
+    # print  "... ... ... ... In likelihood_func_beta ... with mean, variance = ", mean, variance
+    # print  "... ... ... ... ... The new_phase at q based on smearing from physical msmt info at j:", new_phase
+    # print  "... ... ... ... ... The old_phase at q based on posterior QubitGrid at q, at t-1:", old_phase
+    # print  "... ... ... ... ... Likelihood contribution at q: result = ", result
+    # print  "... ... ... ... EXIT likelihood_func_beta ..."
+    # print 
     return result
 
 LIKELIHOOD_BETA = {"l_func": likelihood_func_beta,
@@ -108,7 +109,7 @@ LIKELIHOOD_BETA = {"l_func": likelihood_func_beta,
 def beta_weight_calc(BetaParticle, **args):
     '''docstring'''
     
-    print "... ... ... In beta_weight_calc ..."
+    # print  "... ... ... In beta_weight_calc ..."
     likelihood_neighbours = []
 
     for idx_q in range(len(BetaParticle.neighbourhood_qj)):
@@ -118,10 +119,10 @@ def beta_weight_calc(BetaParticle, **args):
         likelihood_neighbours.append(likelihood)
 
     net_likelihood = np.prod(np.asarray(likelihood_neighbours).flatten())
-    print "... ... ... The likelihood over all qubits in beta neighbourhood is:", likelihood_neighbours
-    print "... ... ... The net_likelihood is:", net_likelihood
-    print "... ... ... EXIT beta_weight_calc"
-    print
+    # print  "... ... ... The likelihood over all qubits in beta neighbourhood is:", likelihood_neighbours
+    # print  "... ... ... The net_likelihood is:", net_likelihood
+    # print  "... ... ... EXIT beta_weight_calc"
+    # print 
     return net_likelihood
 
 

particlesets.py
@@ -34,21 +34,28 @@ class Particle(object):
 class BetaParticle(Particle):
     '''docstring'''
 
-    def __init__(self, node_j, parent_state):
+    def __init__(self, node_j, parent_state, radius):
         Particle.__init__(self)
 
         self.parent = parent_state
         self.particle = np.asarray(parent_state).flatten() # intiialised identically to parent
         self.total_nodes = int(float(len(parent_state)) / 4.0)
 
+        # print  
+        # print  "I'm a beta particle  in  BetaParticle, and my parent state is", parent_state
+        # print  
+        
         self.node_j = node_j
         self.neighbourhood_qj = []
         self.neighbour_dist_qj = []
         self.smeared_phases_qj = []
 
-        self.x_j, self.y_j, self.f_j, self.r_j = self.parent[self.node_j::self.total_nodes]
+        self.x_j, self.y_j, self.f_j, self.r_j = self.particle[self.node_j::self.total_nodes]
 
-        self.mean_radius = self.r_j*3.0 #mean_radius_j # TODO: Change to self.r_j*3.0
+        self.mean_radius = radius #*3.0  # self.r_j*3.0  # equivalent statements
+        # print "Beta particle with radii = radius ", radius
+        # print "Beta particle intiated with radii = radius ", self.mean_radius
+
 
     def get_neighbourhood_qj(self):
         '''doc string'''
@@ -58,13 +65,15 @@ class BetaParticle(Particle):
 
         for idx in range(self.total_nodes):
 
-            xq_ = self.parent[idx]
-            yq_ = self.parent[idx + self.total_nodes]
+            xq_ = self.particle[idx]
+            yq_ = self.particle[idx + self.total_nodes]
             dist = np.sqrt((xq_ - self.x_j)**2 + (yq_ - self.y_j)**2)
 
             if dist <= self.mean_radius:
-                self.neighbourhood_qj.append(idx)
-                self.neighbour_dist_qj.append(dist)
+
+                if dist > 0.0:
+                    self.neighbourhood_qj.append(idx)
+                    self.neighbour_dist_qj.append(dist)
 
 
     def smear_fj_on_neighbours(self, **args):
@@ -97,13 +106,19 @@ class AlphaParticle(Particle):
         Particle.__init__(self)
         self.pset_beta = 0
         self.node_j = 0.0
+        self.SIG2_MEASR = 0.0
         # self.mean_radius_j = 0.0 # TODO FIX THIS. CANT BE ONE NUMBER FORALL j
         self.BetaAlphaSet_j = None
 
-    def generate_beta_pset(self, parents): #, number_of_beta_particles):
+    def generate_beta_pset(self, parents, radii): #, number_of_beta_particles):
         '''docstring'''
-        beta_s = [BetaParticle(self.node_j, state) for state in parents]
+        beta_s = []
+        for idx in range(len(parents)):
+            state = parents[idx]
+            radius = radii[idx]
+            beta_s.append(BetaParticle(self.node_j, state, radius))
         self.BetaAlphaSet_j = ParticleSet(beta_s, **WEIGHTFUNCDICT_BETA)
+        # print "generate_beta_pset...."
         # return BetaAlphaSet
 
 class ParticleSet(object):