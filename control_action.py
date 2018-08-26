Bug testing. (1) Posterior weights are all zero if the sigmoid error variance is too low. (2) Controller was re-coded to ensure sensible control neighbouhoods were being tested. (3) Initialisation of particles was revisited: f_state cannot be initialised in PF but only when a new alpha particle is initiated as it cannot have a setter function (only read values via a getter function). (4) Lots of print debugging! Result: key bug unresolved: some Alpha nodes fail to generate Beta layer after multiple iterations.

control_action.py
@@ -20,12 +20,12 @@ Created on Thu Apr 20 19:20:43 2017
 
 import numpy as np
 
-def control_lengthscale_uncertainty(listofcontrolparameters, 
+def control_lengthscale_uncertainty(listofcontrolparameters,
                                     next_control_neighbourhood,
-                                    number_of_nodes,
-                                    dtype = [('Node', int), ('ControlParam', float)]
-                                    ):
-# def control_lengthscale_uncertainty(listofcontrolparameters, number_of_nodes):
+                                    number_of_diff_nodes=1,
+                                    dtype=[('Node', int), ('ControlParam', float)]
+                                   ):
+
     ''' Return location(s) for next set of single qubit measurement(s)
         selecting qubit locations with highest state estimate uncertainty.
 
@@ -38,9 +38,13 @@ def control_lengthscale_uncertainty(listofcontrolparameters,
         next_control_neighbourhood (`int`| list) :
             List of indices for a qubit within a control region.
 
-        number_of_nodes (`int`| scalar):
-            Number of single qubit measurements that can be simultaneously
-            performed on the hardware grid.
+        number_of_diff_nodes (`int`| scalar | optional):
+            Number of single qubit measurements at different locations
+            that can be simultaneously performed on the hardware grid.
+
+        dtype ( List of tuples | optional) :
+            Specifies how control parameters and control neighbourhoods are read,
+            and handled by this function. Should be a hidden local variable.
 
     Returns:
     -------
@@ -49,29 +53,32 @@ def control_lengthscale_uncertainty(listofcontrolparameters,
             Dims: number_of_nodes
     '''
 
+    # COMMENT: store control parameters in a structureed numpy array.
+    # Then, mask control parameters which are corresspond to be out of the
+    # control neighbourhood. If the control is empty, include all qubits
+    # in the analysis for choosing the next measurement.
+
     labelled_params = [iterate for iterate in enumerate(listofcontrolparameters)]
     structured_array = np.asarray(labelled_params, dtype=dtype)
-    # Take out those parameters not in the control region
-    # Assume that the Qubit Grid is convex. Any convex polygon can be triangulated
-    # Use this to justify, consider only three nearest neighbours in picking from neighbourhood. (No three point co-linear)
-    # This argument may not apply but its a start
 
-    if 3 <= len(next_control_neighbourhood):
-        print "the neighbourhood has at least 3 qubits"
-        if len(next_control_neighbourhood) < len(listofcontrolparameters) - 3:
-            print "the neighbourhood is very large"
-            mask = np.zeros(len(labelled_params), dtype=bool) # Mask all values
-            mask[next_control_neighbourhood] = True # Show nodes only from next_control_neighbourhood
-            structured_array = structured_array[mask]#No mask for lists - make array and bring back
-            print "I truncated something, see that the control params are shorter", len(structured_array)
+    if len(next_control_neighbourhood) == 0:
+        mask = np.ones(len(labelled_params), dtype=bool) # Mask for showing all values.
+        print "Control List empty"
+
+    if len(next_control_neighbourhood) > 0:
+        mask = np.zeros(len(labelled_params), dtype=bool) # Mask for hiding all values.
+        mask[next_control_neighbourhood] = True # Show nodes only from next_control_neighbourhood.
+
+    structured_array = structured_array[mask] # No mask for lists - make array and bring back.
 
     # Now, sort the region from max to min uncertainty.
-    # structured_array = np.asarray(labelled_params, dtype=dtype)
     sorted_array = np.sort(structured_array, order=['ControlParam', 'Node'])[::-1]
 
-    # Find multiple instances of maximal uncertainty
+    # Zeroth term of sorted_array is the node corresponding to maximal ucertainity.
     max_val = sorted_array['ControlParam'][0]
-    print "max value is ", max_val, ", at node, ", sorted_array['Node'][0]
+
+    # Now find multiple instances of the maximum value if they exist.
+    # If the counter > 1, then multiple maxima exist.
     counter = 1
     for controlparamval in sorted_array['ControlParam'][1:]:
         if controlparamval == max_val:
@@ -81,41 +88,14 @@ def control_lengthscale_uncertainty(listofcontrolparameters,
 
     # Return controls based on highest uncertainty.
     # If equi-certain options exist, choose between these options at random.
-    number_of_nodes = 2
-    if number_of_nodes < counter:
+    if number_of_diff_nodes < counter:
         # Returns random choices among equicertain nodes.
-        chosen_node_indices = np.random.randint(low=0, high=counter, size=number_of_nodes)
+        chosen_node_indices = np.random.randint(low=0, high=counter, size=number_of_diff_nodes)
         return sorted_array['Node'][chosen_node_indices]
 
     elif counter <= number_of_nodes:
-        # Returns nodes in descending order of uncertainty.
-        return sorted_array['Node'][0 : number_of_nodes]
-
-    # controls_list = []
-    # print "here is the input list of r-variances for control"
-    # print listofcontrolparameters
-
-    # for idxnode in range(number_of_nodes):
-
-    #     # TODO: enumerate and sort?
-    #     node_j = np.argmax(listofcontrolparameters)
-
-    #     if  node_j > -1.0:
-    #         # TODO: Eliminate node from future potential controls - why?
-    #         listofcontrolparameters[node_j] = -1.0
-    #         controls_list.append(node_j)
-
-    #     elif node_j == -1.0:
-
-    #         if len(controls_list) > 0:
-    #             return controls_list
-
-    #         elif len(controls_list) == 0:
-    #             print "ERROR in control_lengthscale_uncertainty"
-    #             print "List of controls  = ", controls_list
-    #             raise RuntimeError
-
-    # return np.asarray(controls_list)
+        # Return nodes in descending order of uncertainty
+        return sorted_array['Node'][0 : number_of_diff_nodes]
 
 
 def control_user_input(input_controls, next_control_neighbourhood):
@@ -168,4 +148,3 @@ def controller(listofcontrolparameters, next_control_neighbourhood, controltype=
 
         print "Input control parameters empty."
         raise RuntimeError
-

hardware.py
@@ -17,7 +17,7 @@ Created on Thu Apr 20 19:20:43 2017
 .. moduleauthor:: Riddhi Gupta <riddhi.sw@gmail.com>
 '''
 import numpy as np
-from qslamdesignparams import MODELDESIGN
+from qslamdesignparams import MODELDESIGN,PRIORDICT
 
 LAMBDA = MODELDESIGN["LAMBDA_1"]
 PARTICLE_STATE = ["x_state", "y_state", "f_state", "r_state"]
@@ -89,7 +89,10 @@ class Node(object):
 
     def __init__(self):
 
-        self._f_state = 0.0
+        map_sample = PRIORDICT["SAMPLE_F"]["FUNCTION"](**PRIORDICT["SAMPLE_F"]["ARGS"])
+        print "Initialised map as", map_sample
+        self._f_state = map_sample
+
         self.r_state = 0.0
         self.__r_state_variance = 10**4 # COMMENT: default value. Very large to make threshold.
         self.x_state = 0.0
@@ -139,16 +142,20 @@ class Node(object):
     @property
     def f_state(self): # no .setter function
         '''Return state variable for the dephasing noise field about this location.'''
+
         prob_sample = self.sample_prob_from_msmts()
+
+        # print "Inside f_state, the probability sample is:", prob_sample
+
         if prob_sample is None:
-            # self._f_state = np.random.uniform(low=0.0, high=np.pi)
-            # print "Map value returned is default; prob_sample NONE in f_state", self._f_state
+            # print "Map value returned is I.C.; prob_sample NONE in f_state", self._f_state
             return self._f_state
+
         if prob_sample >= 0.0 and prob_sample <= 1.0:
             self._f_state = Node.inverse_born(prob_sample)
             return self._f_state
-        # print "INVALID prob_sample value encountered in calling f_state", prob_sample
-        print prob_sample
+
+        print "INVALID prob_sample value encountered in calling f_state", prob_sample
         raise RuntimeError
 
     def sample_prob_from_msmts(self): # TODO Data Association
@@ -172,18 +179,20 @@ class Node(object):
         if w_p is None and w_q is None:
             # print "NONE returned in sample_prob_from_msmts"
             return  None
+        
         elif w_p is not None  and w_q is None:
             w_p = 1.0
             w_q = 0.0
         elif w_p is None and w_q is not None:
-            w_q = 1.0
             w_p = 0.0
+            w_q = 1.0
         elif w_p is not None and w_q is not None:
             pass
         prob_j = w_p*prob_p + w_q*prob_q
 
         if prob_j > 1 or prob_j < 0:
             raise RuntimeError
+        
         return prob_j
 
 

particlesets.py
@@ -136,14 +136,22 @@ class ParticleSet(object):
         self.w_dict = WEIGHTFUNCDICT
         self.weights_set = (1.0 / self.p_set)*np.ones(self.p_set)
 
+
     def calc_weights_set(self):
         '''Return an array of normalised weights for a list of Particle objects.'''
         new_weight_set = []
+        
+        print # TODO : Delete code. Printdebug only.
+        print "in calc_weight_set " # TODO : Delete code. Printdebug only.
+        print "printting dictionary" # TODO : Delete code. Printdebug only.
+        print self.w_dict["args"] # TODO : Delete code. Printdebug only.
+        print # TODO : Delete code. Printdebug only.
+
         for particle in self.particles:
             new_weight = self.w_dict["function"](particle, **self.w_dict["args"])
 
             if new_weight > MAX_WEIGHT_CUTOFF: # avoid inf
-                print "Large weight reset"
+                # print "Large weight reset"
                 new_weight = float(MAX_WEIGHT_CUTOFF)
 
             new_weight_set.append(new_weight)
@@ -153,7 +161,7 @@ class ParticleSet(object):
         unnormalised_total = np.sum(raw_weights)
 
         if unnormalised_total == 0.0: # avoid zeros
-            print "Weights all zeros =", raw_weights
+            # print "Weights all zeros =", raw_weights
             normalisation = 1.0
             return normalisation*raw_weights
 
@@ -319,7 +327,9 @@ class BetaParticle(Particle):
         ''' Builds smeared_phases_qj, the list of phase estimates for all
             neighbouring qubits around node_j, after a local measurement at node_j.
         '''
-
+        # print  # TODO : Delete code. Printdebug only.
+        # print "In particlesets.smear_fj_on_neighbours..." # TODO : Delete code. Printdebug only.
+        
         self.get_neighbourhood_qj()
 
         prev_posterior_f_state = args["prev_posterior_f_state"]
@@ -332,10 +342,22 @@ class BetaParticle(Particle):
             node_q = self.neighbourhood_qj[idx_q]
             dist_jq = self.neighbour_dist_qj[idx_q]
             tau_q = prev_counter_tau_state[node_q]
+
+            if tau_q == 0:
+                # TODO: Add functionality here for dealing with unmeasured qubits.
+                pass
             f_state_q = prev_posterior_f_state[node_q]
 
             lambda_q = lambda_** tau_q
             kernel_val = args["kernel_function"](dist_jq, self.f_j, self.r_j)
 
             smear_phase = (1.0 - lambda_q)*f_state_q + lambda_q*kernel_val
+
+            # print "Measured qubit j - map value, lengscale", self.f_j, self.r_j # TODO : Delete code. Printdebug only.
+            # print 'Distance between qubits', dist_jq # TODO : Delete code. Printdebug only.
+            # print "Value from the kernel", kernel_val # TODO : Delete code. Printdebug only.
+            # print "Previosu posterior state at q", f_state_q # TODO : Delete code. Printdebug only.
+            # print "Smeared phase: ", smear_phase # TODO : Delete code. Printdebug only.
+            # print # TODO : Delete code. Printdebug only.
+
             self.smeared_phases_qj.append(smear_phase)

particleweightcalcs.py
@@ -228,7 +228,8 @@ def beta_weight_calc(BetaParticle, **args):
     '''
 
     likelihood_neighbours = []
-
+    # print # TODO : Delete code. Printdebug only.
+    # print "In beta_weight_calc" # TODO : Delete code. Printdebug only.
     for idx_q in range(len(BetaParticle.neighbourhood_qj)):
         # INCORRECT? SHOULDN"T THIS BE args["l_args"]["new_phase"]
         # args["new_phase"] = BetaParticle.smeared_phases_qj[idx_q]
@@ -237,8 +238,11 @@ def beta_weight_calc(BetaParticle, **args):
         # INCORRECT? SHOULDN"T THIS BE args["l_args"]["old_phase"]
         # args["old_phase"] = BetaParticle.parent[idx_q]
         likelihood = args["l_func"](**args["l_args"])
-        likelihood_neighbours.append(likelihood)
-
+        likelihood_neighbours.append(likelihood) 
+        # print "For the next beta likelhood calculation,..." # TODO : Delete code. Printdebug only.
+        # print "LIKELHOOD BETA args are: ", args["l_args"] # TODO : Delete code. Printdebug only.
+        # print "And the likelihood value is ", likelihood # TODO : Delete code. Printdebug only.
+        # print # TODO : Delete code. Printdebug only.
     net_likelihood = np.prod(np.asarray(likelihood_neighbours).flatten())
     return net_likelihood
 

qslamdesignparams.py
@@ -116,7 +116,7 @@ GRIDDICT = {"QUBIT_1" : (1., 0.0),
 
 ####################################### PROCESS AND MEASUREMENT NOISE ##########
 
-NOISEPARAMS = {"SIGMOID_APPROX_ERROR" : {"MU": 0.0, "SIGMA" : (np.pi*0.01)**2},
+NOISEPARAMS = {"SIGMOID_APPROX_ERROR" : {"MU": 0.0, "SIGMA" : 1.0},
                "QUANTISATION_UNCERTY" : {"MU": 0.0, "SIGMA" : 0.001},
                "CORRELATION_DYNAMICS" : {"MU": 0.0, "SIGMA" : 0.0},
                "SPATIAL_NOISE_JITTER" : {"MU": 0.0, "SIGMA" : 0.0}
@@ -135,11 +135,11 @@ FIX_LAMBDAS = 0.99
 MODELDESIGN = {"LAMBDA_1" : FIX_LAMBDAS, # Forgetting factor for sample probabilities
                "LAMBDA_2" : FIX_LAMBDAS, # Forgetting factor for smeare phases
                "GAMMA_T" : 100000.0, # Re-sampling threshold -- NOT USED
-               "P_ALPHA" : 20, # Number of alpha particles
-               "P_BETA" : 40, # Numer of beta particles for each alpha
+               "P_ALPHA" : 5, # Number of alpha particles
+               "P_BETA" : 3, # Numer of beta particles for each alpha
                "kernel_function" : gaussian_kernel,
                "MULTIPLER_R_MIN" : 1.0,  # Sets R_Min based on qubit grid.
-               "MULTIPLER_R_MAX" : 10.0, # Sets R_Max based on qubit grid.
+               "MULTIPLER_R_MAX" : 2.0, # Sets R_Max based on qubit grid.
                "MULTIPLER_MEAN_RADIUS" : 1.0, # Sets mean radius according to posterior r_state??
                "MAX_WEIGHT_CUTOFF" : 100000.0,
                "MSMTS_PER_NODE" : 1
@@ -160,8 +160,8 @@ def sample_s_prior(**args):
                                size=SIZE)
     return samples
 
-S_PRIOR_ARGS = {"MEAN" : None,
-                "VAR" : None,
+S_PRIOR_ARGS = {"MEAN" : 0.0,
+                "VAR" : 10.0**(-8),
                 "SIZE" : None}
 
 